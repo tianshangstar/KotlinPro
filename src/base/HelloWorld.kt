c9e17278ea3ad2abd9cfbb72c10116a3cd618f05
evan01.zhang <evan01.zhang@vipshop.com>
2018-04-18 17:13:27 +0800

    国际惯例 helloworld

---
+++ src/base/HelloWorld.kt
@@ -0,0 +1,14 @@
+package base
+
+// 可选包头
+
+/**
+ * Created by evan01.zhang on 2018/4/12.
+ */
+fun main(args: Array<String>) {
+    //fun -> 方法定义
+    //main-> 方法名
+    //args->入参
+    //Array<String> -> 入参类型
+    println("hello world")
+}
